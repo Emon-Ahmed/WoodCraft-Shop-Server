66c4a69d893fd59ae988dd3125e138b9fd60b89c
Emon Ahmed <technicalboyemon@gmail.com>
2021-11-10 16:19:23 +0600

    Fxig

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# niche-website-server-side-technicalboyemon
+# WoodCraft Shop (Server)
+Live demo [_Click Here_](#)
+
+```
+https://woodcraft-shop.web.app/
+```
+
+
+# Project Details
+
+## Table of Contents
+* [Project Info](#project-information)
+* [Features](#features)
+* [Acknowledgements](#acknowledgements)
+* [Contact](#contact)
+
+
+## Project Information
+- We Are Here To Build World's Most Biggest Project, Called WoodCraft Shop
+
+
+## Features and Functionality
+List The Ready Features Here:
+- You Can Find Best Product Here
+- Also You Can Easily Navigate The Navbar
+- This Design is Followed By "Less is More"
+
+
+
+## Acknowledgements
+Give Credit Here
+- This Assignment was Provided By [Programming-Hero](https://web.programming-hero.com/)
+- Many thanks To Programming-Hero Team
+
+
+## Contact
+Created by [@Emon Ahmed](https://www.emonahmed.com/)
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uana9.mongodb.net/DhakaTravelAgencyDB?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
